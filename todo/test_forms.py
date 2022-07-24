6cbe046d20c65d1cf47887703851838860e2c76c
AriRix1410 <aa.fraserhaste@gmail.com>
2022-07-24 22:07:52 +0000

    Prepare to deploy to Heroku

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -2,9 +2,6 @@ from django.test import TestCase
 from .forms import ItemForm
 
 
-# Create your tests here.
-
-
 class TestItemForm(TestCase):
 
     def test_item_name_is_required(self):
@@ -14,11 +11,9 @@ class TestItemForm(TestCase):
         self.assertEqual(form.errors['name'][0], 'This field is required.')
 
     def test_done_field_is_not_required(self):
-        form = ItemForm({'name': 'Test Todo Items'})
+        form = ItemForm({'name': 'Test Todo Item'})
         self.assertTrue(form.is_valid())
 
     def test_fields_are_explicit_in_form_metaclass(self):
         form = ItemForm()
         self.assertEqual(form.Meta.fields, ['name', 'done'])
-
-
--- todo/test_views.py
+++ todo/test_views.py
@@ -1,8 +1,6 @@
 from django.test import TestCase
 from .models import Item
 
-# Create your tests here.
-
 
 class TestViews(TestCase):
 
@@ -40,3 +38,9 @@ class TestViews(TestCase):
         updated_item = Item.objects.get(id=item.id)
         self.assertFalse(updated_item.done)
 
+    def test_can_edit_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.post(f'/edit/{item.id}', {'name': 'Updated Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
